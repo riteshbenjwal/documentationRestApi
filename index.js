850a67d9dcfbf26024f1ed50f134eebe4b67c16e
Ritesh Benjwal <riteshbenjwal7@gmail.com>
2021-11-22 19:40:53 +0530

    OBJECT_ARRAY_DOCS_DONE

--- index.js
+++ index.js
@@ -10,10 +10,44 @@ const swaggerDocument = YAML.load('./swagger.yaml');
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 
+let courses = [
+    {
+        id: "11",
+        name: "React",
+        price: 200
+    },
+    {
+        id:  "22",
+        name: "Angular",
+        price: 399
+    },
+    {
+        id: "33",
+        name: "Django",
+        price: 499
+    }
+]
+
 
 app.get("/", (req, res)=>{
     res.send("Hello World");
 })
 
+app.get("/api/v1/rc", (req,res)=>{
+  res.send("Hello from Rc docs")
+})
+
+app.get("/api/v1/rcobject", (req,res)=>{
+    res.send({
+        id: "55",
+        name: "Backend",
+        price: 999
+    })
+  })
+  
+
+  app.get("/api/v1/courses", (req,res)=>{
+    res.send(courses)
+  })
 
 app.listen(4000, () => console.log("Server is running on port 4000"));
